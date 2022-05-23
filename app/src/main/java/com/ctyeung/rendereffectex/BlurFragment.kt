bb490a958e9b23dc96c236eb641dd4c540f21da4
Chi Yeung <yeun0007@umn.edu>
2022-05-22 19:57:22 -0500

    blur
    blur filter

--- app/src/main/java/com/ctyeung/rendereffectex/BlurFragment.kt
+++ app/src/main/java/com/ctyeung/rendereffectex/BlurFragment.kt
@@ -1,12 +1,21 @@
 package com.ctyeung.rendereffectex
 
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
 
 class BlurFragment : Fragment() {
+
+    companion object {
+
+        const val DEFAULT_BLUR = 10f
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +27,25 @@ class BlurFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blur, container, false)
+        val view = inflater.inflate(R.layout.fragment_blur, container, false)
+        view.findViewById<CheckBox>(R.id.chk_blur)?.apply {
+            isChecked = false
+
+            setOnClickListener {
+                val imageView = view.findViewById<ImageView>(R.id.image_view)
+                applyBlur(imageView, isChecked)
+            }
+        }
+        return view
     }
 
+    private fun applyBlur(imageView:ImageView, isOn:Boolean) {
+        val blurEffect = if(isOn) {
+            RenderEffect.createBlurEffect(DEFAULT_BLUR, DEFAULT_BLUR, Shader.TileMode.MIRROR)
+        }
+        else {
+            null
+        }
+        imageView.setRenderEffect(blurEffect)
+    }
 }
